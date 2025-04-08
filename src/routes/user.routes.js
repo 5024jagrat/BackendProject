1352ffa6e91bd19cc7f706861c885858874ac836
Jagrat parnami <parnami.jagrat.com>
2025-04-08 17:46:43 +0530

    Pipelines and methods complete for videotube

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,11 +3,19 @@ import {
   loginUser,
   registerUser,
   logoutUser,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserProfile,
+  watchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 import { refreshAcesstoken } from "../controllers/user.controller.js";
+import { verify } from "jsonwebtoken";
+
 const router = Router();
 
 router.route("/register").post(
@@ -29,6 +37,21 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 
-router.route("/refresh-token").post(refreshAcesstoken)
+router.route("/refresh-token").post(refreshAcesstoken);
+
+router.route("/changepassword").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avtar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserProfile);
+
+router.route("/history").get(verifyJWT, watchHistory);
 
 export default router;
